49d2247fd860a0c62bca53ccc00b3a61d4015544
Ivan Vashchinskiy <ivashkakiller228@outlook.com>
2023-04-16 23:32:58 +0700

    first commit

---
+++ WindowsFormsApp2/8 лаб.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class EmployeeSearchForm1 : System.Windows.Forms.Form
+    {
+        private readonly Employee[] employees;
+
+        public EmployeeSearchForm1()
+        {
+            InitializeComponent();
+
+            // Создание массива сотрудников и заполнение его данными
+            employees = new Employee[]
+            {
+                new Employee { Name = "Иван", Surname = "Иванов", Position = "Менеджер", HireDate = new DateTime(2020, 1, 1) },
+                new Employee { Name = "Петр", Surname = "Петров", Position = "Программист", HireDate = new DateTime(2021, 5, 1) },
+                new Employee { Name = "Сергей", Surname = "Сергеев", Position = "Дизайнер", HireDate = new DateTime(2019, 8, 1) },
+            };
+            
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string surname = surnameTextBox.Text;
+            string position = positionTextBox.Text;
+            DateTime hireDate = hireDateTimePicker.Value;
+
+            Employee[] results;
+
+            if (!string.IsNullOrEmpty(surname))
+            {
+                results = EmployeeHelper.FindBySurname(employees, surname);
+            }
+            else if (!string.IsNullOrEmpty(position))
+            {
+                results = EmployeeHelper.FindByPosition(employees, position);
+            }
+            else if (hireDate != DateTime.MinValue) // проверка на изменение значения DateTimePicker
+            {
+                results = EmployeeHelper.FindByHireDate(employees, hireDate);
+            }
+            else
+            {
+                results = new Employee[0];
+            }
+
+            resultsListBox.Items.Clear();
+
+            foreach (Employee employee in results)
+            {
+                resultsListBox.Items.Add($"{employee.Surname} {employee.Name}, {employee.Position}, {employee.HireDate.ToShortDateString()}");
+            }
+        }
+
+
+        private void surnameTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                searchButton_Click(sender, e);
+                e.Handled = true;
+            }
+        }
+
+        private void positionTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                searchButton_Click(sender, e);
+                e.Handled = true;
+            }
+        }
+
+        private void hireDateTimePicker_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                searchButton_Click(sender, e);
+                e.Handled = true;
+            }
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Position { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+
+    public static class EmployeeHelper
+    {
+        public static Employee[] FindBySurname(Employee[] employees, string surname)
+        {
+            return employees.Where(e => e.Surname == surname).ToArray();
+        }
+
+        public static Employee[] FindByPosition(Employee[] employees, string position)
+        {
+            return employees.Where(e => e.Position == position).ToArray();
+        }
+
+        public static Employee[] FindByHireDate(Employee[] employees, DateTime hireDate)
+        {
+            return employees.Where(e => e.HireDate == hireDate).ToArray();
+        }
+    }
+}
---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class EmployeeSearchForm1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.surnameTextBox = new System.Windows.Forms.TextBox();
+            this.positionTextBox = new System.Windows.Forms.TextBox();
+            this.hireDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.resultsListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // surnameTextBox
+            // 
+            this.surnameTextBox.Location = new System.Drawing.Point(60, 87);
+            this.surnameTextBox.Name = "surnameTextBox";
+            this.surnameTextBox.Size = new System.Drawing.Size(200, 20);
+            this.surnameTextBox.TabIndex = 0;
+            this.surnameTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.surnameTextBox_KeyDown);
+            // 
+            // positionTextBox
+            // 
+            this.positionTextBox.Location = new System.Drawing.Point(60, 147);
+            this.positionTextBox.Name = "positionTextBox";
+            this.positionTextBox.Size = new System.Drawing.Size(200, 20);
+            this.positionTextBox.TabIndex = 1;
+            this.positionTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.positionTextBox_KeyDown);
+            // 
+            // hireDateTimePicker
+            // 
+            this.hireDateTimePicker.Location = new System.Drawing.Point(60, 208);
+            this.hireDateTimePicker.Name = "hireDateTimePicker";
+            this.hireDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.hireDateTimePicker.TabIndex = 2;
+            this.hireDateTimePicker.KeyDown += new System.Windows.Forms.KeyEventHandler(this.hireDateTimePicker_KeyDown);
+            // 
+            // resultsListBox
+            // 
+            this.resultsListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultsListBox.FormattingEnabled = true;
+            this.resultsListBox.ItemHeight = 16;
+            this.resultsListBox.Location = new System.Drawing.Point(339, 81);
+            this.resultsListBox.Name = "resultsListBox";
+            this.resultsListBox.Size = new System.Drawing.Size(222, 132);
+            this.resultsListBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите фамилию";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите должность";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Введите дату";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // EmployeeSearchForm1
+            // 
+            this.ClientSize = new System.Drawing.Size(619, 371);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultsListBox);
+            this.Controls.Add(this.hireDateTimePicker);
+            this.Controls.Add(this.positionTextBox);
+            this.Controls.Add(this.surnameTextBox);
+            this.Name = "EmployeeSearchForm1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox surnameTextBox;
+        private System.Windows.Forms.TextBox positionTextBox;
+        private System.Windows.Forms.DateTimePicker hireDateTimePicker;
+        private System.Windows.Forms.ListBox resultsListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
